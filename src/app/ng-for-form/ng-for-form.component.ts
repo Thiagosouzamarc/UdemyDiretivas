000d9ab4b4e9b4030681d432fb882a47212a6fb4
Thiago Souza Marciano <thiagomarciano@brq.com>
2019-08-19 18:45:44 -0300

    19-08

---
+++ src/app/ng-for-form/ng-for-form.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-for-form',
+  templateUrl: './ng-for-form.component.html',
+  styleUrls: ['./ng-for-form.component.css']
+})
+export class NgForFormComponent implements OnInit {
+
+  name: string ="";
+  address: string ="";
+  phone: string ="";
+  city: string ="";
+  age: number = 0;
+
+  cityes = [
+    {name: "São Paulo", state: "SP"},
+    {name: "Porto Alegre", state: "RS"},
+    {name: "Curitiba", state: "PR"},
+    {name: "Rio de Janeiro", state: "RJ"},
+  ]
+  
+  clients=[];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  save(){
+    this.clients.push({
+      name: this.name,
+      address: this.address,
+      city: this.city,
+      age: this.age,
+      phone: this.phone
+    })
+    this.cancel();
+    console.log('fff', this.clients)
+  }
+
+  cancel(){
+    this.name = "";
+    this.address = "";
+    this.city = "";
+    this.phone = "";
+    this.age = null;
+  }
+
+  delete(i: number){
+    this.clients.splice(i, 1);
+  }
+
+
+
+}
